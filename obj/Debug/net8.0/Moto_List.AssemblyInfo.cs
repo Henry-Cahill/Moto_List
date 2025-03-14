5be570c2bd6b71dd68972db779dd93fb7fd5e7b7
HowTooSoftware <hlcmailval2021@gmail.com>
2025-03-13 22:21:00 -0500

    Menu redue
    I didn't like how my menu looked just modified the menu to look spectacular.

--- obj/Debug/net8.0/Moto_List.AssemblyInfo.cs
+++ obj/Debug/net8.0/Moto_List.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Moto_List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+406d6c0ea07f4e5fb5cbe6902457dd042cf494ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3989a6ac19f2d87a61b1d819adb6f51be477729")]
 [assembly: System.Reflection.AssemblyProductAttribute("Moto_List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Moto_List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
